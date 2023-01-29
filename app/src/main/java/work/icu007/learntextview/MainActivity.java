ffb3ac825e2a391d3c493b019891c32e6b09b4db
CharlieLiao <2826227445@qq.com>
2023-01-29 15:46:25 +0800

    Initial commit

---
+++ app/src/main/java/work/icu007/learntextview/MainActivity.java
@@ -0,0 +1,14 @@
+package work.icu007.learntextview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
